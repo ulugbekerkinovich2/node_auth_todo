f2cc59163f5f446a5bb5a0e110168d7b6ebb9264
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2024-05-17 18:49:45 +0500

    first commit

---
+++ src/start/modules.js
@@ -0,0 +1,19 @@
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const authRoute = require("../routes/аuth.route");
+const usersRoute = require("../routes/users.route");
+const todoRoute = require("../routes/todo.route");
+const isAuthMiddleware = require("../middlewares/is-auth.middleware")
+
+const modules = (app, express) => {
+    app.use(cors());
+    app.use(fileUpload());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(`${process.cwd()}/uploads`));
+    app.use("/api/auth", authRoute);
+    app.use("/api/users", usersRoute);
+    app.use("/api/todos", todoRoute);
+};
+
+module.exports = modules;
